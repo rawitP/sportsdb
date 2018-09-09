d8a2d4033c9404299f03cd01537a82a84e08215d
rawitP <rawit.p@ku.th>
2018-09-09 23:23:22 +0700

    Add - Update & Delete Articles

--- app.js
+++ app.js
@@ -80,6 +80,47 @@ app.post('/articles/add', function(req, res) {
     });
 });
 
+// Load Edit Form
+app.get('/article/edit/:id', function(req, res) {
+    Article.findById(req.params.id, function(err, article) {
+        res.render('edit_article', {
+            title: 'Edit Article',
+            article: article
+        });
+    });
+});
+
+// Update Submit
+app.post('/articles/edit/:id', function(req, res) {
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id:req.params.id};
+
+    Article.update(query, article, function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+app.delete('/article/:id', function(req, res) {
+    console.log(req.params.id);
+    let query = {_id:req.params.id};
+
+    Article.remove(query, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        res.send('Success');
+    });
+});
+
 app.listen(3000, function() {
     console.log('Server started on port 3000...');
 });
