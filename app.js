23cbb854c091f079fa0dbfaa8cbc52a2c574e55e
rawitP <rawit.p@ku.th>
2018-09-09 09:34:53 +0700

    Set up a PugJS and nodemon

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 const express = require('express');
+const path = require('path');
 
+// Init App
 const app = express();
 
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    let articles = [
+        {
+            id: 1,
+            title: 'Article One',
+            author: 'Rawit Panjaroen',
+            body: 'This is article one'
+        },
+        {
+            id: 2,
+            title: 'Article Two',
+            author: 'Rawit Panjaroen',
+            body: 'This is article two'
+        },
+        {
+            id: 3,
+            title: 'Article Three',
+            author: 'Rawit Panjaroen',
+            body: 'This is article three'
+        }
+    ]
+    res.render('index', {
+        title: 'Articles',
+        articles: articles
+    });
+});
+
+app.get('/articles/add', function(req, res) {
+    res.render('add_article', {
+        title: 'Add Article'
+    });
 });
 
 app.listen(3000, function() {
