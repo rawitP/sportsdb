c2eb3e25f7226c899b893ba5911a6dc3354e40fe
rawitP <rawit.p@ku.th>
2018-09-09 21:27:53 +0700

    Add - Save Articles to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/sportdb');
 let db = mongoose.connection;
@@ -25,6 +26,11 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}));
+// parse application/json
+app.use(bodyParser.json());
+
 // Home Route
 app.get('/', function(req, res) {
     Article.find({}, function(err, articles) {
@@ -42,6 +48,23 @@ app.get('/', function(req, res) {
 app.get('/articles/add', function(req, res) {
     res.render('add_article', {
         title: 'Add Article'
+    });
+});
+
+// Add Submit POST Route
+app.post('/articles/add', function(req, res) {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
     });
 });
 
